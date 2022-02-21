986359f0722dd3a23f8c3bad8211f2605ee93925
Dyatiend <readysteel@ya.ru>
2022-02-21 17:24:58 +0300

    krygin_2

--- RobotActionListener.java
+++ RobotActionListener.java
@@ -4,4 +4,5 @@ import java.util.EventListener;
 
 public interface RobotActionListener extends EventListener {
     void robotMadeMove(RobotActionEvent e);
+	void destroyedAllHumans(RobotActionEvent e);
 }
